6ef684e7e5da4b677965125b12137adabc158bc1
Shakeb Ameen <shakebameen@gmail.com>
2019-04-10 23:10:08 +0800

    Init

---
+++ src/test/Interface_Backend.java
@@ -0,0 +1,15 @@
+package test;
+
+
+public class Interface_Backend {
+	
+	static String data[] = new String[10];
+	public static void insert(int index, String name) {
+		
+		
+		data[index] = name;
+		System.out.println("Entered successfully");
+		
+	}
+
+}
